f2719dc104c15d72e5bb1dfcfb61dcabee38e370
haowei.wang <haowei.wang@thoughtworks.com>
2023-02-01 11:13:00 +0800

    constraint-layout

---
+++ app/src/main/java/com/thoughtworks/androidtrain/helloworld/ConstraintActivity.kt
@@ -0,0 +1,11 @@
+package com.thoughtworks.androidtrain.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraint_layout)
+    }
+}
--- app/src/main/java/com/thoughtworks/androidtrain/helloworld/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/helloworld/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.thoughtworks.androidtrain.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn: Button = findViewById(R.id.btnStartAnotherAty)
+        btn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ConstraintActivity::class.java))
+        }
     }
 }
