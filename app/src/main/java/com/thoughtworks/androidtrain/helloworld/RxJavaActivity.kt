d62231691da5fb43884b02339cc8d222ef16ae79
haowei.wang <haowei.wang@thoughtworks.com>
2023-02-08 10:22:06 +0800

    fix a mistake that judge Observer end

--- app/src/main/java/com/thoughtworks/androidtrain/helloworld/RxJavaActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/helloworld/RxJavaActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Button
+import android.widget.Toast
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -26,9 +28,10 @@ class RxJavaActivity : AppCompatActivity() {
 
     private fun initData() {
         val btn: Button = findViewById(R.id.rx_btn)
-        val subscribe: Disposable = Observable.create(ObservableOnSubscribe<String> { emmiter ->
+        val compositeDisposable = CompositeDisposable()
+        Observable.create(ObservableOnSubscribe<String> { emmiter ->
             var i = -1;
-            while (i < 11) {
+            while (i < 10) {
                 i += 1
                 emmiter.onNext(i.toString())
                 SystemClock.sleep(1000)
@@ -39,15 +42,27 @@ class RxJavaActivity : AppCompatActivity() {
             .subscribeOn(Schedulers.io())//指定 subscribe() 发生在 IO 线程【数据处理过程在io】
             .observeOn(AndroidSchedulers.mainThread())//指定的操作（Subscriber 的回调）将在 Android 主线程运行【结果使用在主线程】
             //observer【通过subscribe()与被观察者i创造联系，i一变化，这里就触发】
-            .subscribe { i ->
-                btn.text = i
-                if (btn.text == "the number is 11") {
-                    btn.isEnabled = true
-                    btn.text = "Click"
+            .subscribe(object : Observer<String> {
+                override fun onNext(t: String) {
+                    btn.text = t
                 }
-            }
+
+                override fun onSubscribe(d: Disposable) {
+                    compositeDisposable.add(d)
+                }
+
+                override fun onError(e: Throwable) {
+                    Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT).show();
+                }
+
+                override fun onComplete() {
+                    if (btn.text == "the number is 10") {
+                        btn.isEnabled = true
+                        btn.text = "Click"
+                    }
+                }
+            })
         btn.isEnabled = false
-        composite.add(subscribe)
     }
 
     override fun onDestroy() {
